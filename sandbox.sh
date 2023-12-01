97558ee19396bce05b8bbb3706283b854a41cd60
kctsengh <130540467+kctsengh@users.noreply.github.com>
2023-12-01 11:01:08 +0800

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -4,4 +4,5 @@ sudo apt-get install -y  sshpass
 sudo apt-get install -y terminator
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 sshpass -p  $TOKEN ssh-copy-id -o StrictHostKeyChecking=no root@35.236.136.11
+sudo -- sh -c "echo \"35.236.136.11    gcp\" >> /etc/hosts" 
 
