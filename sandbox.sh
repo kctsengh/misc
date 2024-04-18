96a24e2ef6698c05789935b9a02c8c48c3741d18
kctsengh <130540467+kctsengh@users.noreply.github.com>
2024-04-18 11:51:45 +0800

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -8,5 +8,10 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 sshpass -p  $TOKEN ssh-copy-id -o StrictHostKeyChecking=no root@35.185.170.73
 sudo -- sh -c "echo \"35.185.170.73    k4\" >> /etc/hosts"
 
+cat <<EOT>> ~/.ssh/config
 
+Host k4
+  HostName 35.185.170.73
+  User root
+EOT
 
