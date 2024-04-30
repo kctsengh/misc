c777908c5457c3f802509ca2e638e2d15acbd9ff
kctsengh <130540467+kctsengh@users.noreply.github.com>
2024-04-30 12:58:32 +0800

    Update sandbox.sh

--- sandbox.sh
+++ sandbox.sh
@@ -2,7 +2,10 @@
 sudo apt-get  -y update
 sudo apt-get install -y  sshpass
 sudo apt-get install -y terminator
+if ! [ -f ~/.ssh/id_rsa ]; then
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+fi
+
 
 
 sshpass -p  $TOKEN ssh-copy-id -o StrictHostKeyChecking=no root@35.185.170.73
